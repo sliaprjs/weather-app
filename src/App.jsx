657c3c240d8b89673b1512a349537052d460cf41
sliaprjs <sliaprjs@gmail.com>
2023-05-25 11:03:26 +0300

    add geolocation and info fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import Hero from './components/Hero'
 
+
+
+const initialCoords = [];
+
+navigator.geolocation.getCurrentPosition((position) => {
+  initialCoords[0] = [position.coords.latitude.toFixed(2)];
+  initialCoords[1] = [position.coords.longitude.toFixed(2)];
+});
+
 const App = () => {
+  const [coords, setCoords] = useState(initialCoords);
+  const [weatherInfo, setWeatherInfo] = useState();
+
+  const locationUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&appid=26c391408cfb7f28236135730fbed2c7`;
+
+  useEffect(() => {
+    axios.get(locationUrl).then(response => setWeatherInfo(response.data))
+  }, [])
+
+  console.log(coords, weatherInfo);
+
   return (
     <main className='container'>
-      <Hero/>
+      <Hero info={weatherInfo}/>
     </main>
   )
 }
