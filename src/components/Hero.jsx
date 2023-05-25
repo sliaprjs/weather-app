227665a187c19953f5f2cf0080e795c8b3b26fe6
sliaprjs <sliaprjs@gmail.com>
2023-05-25 21:46:19 +0300

    fix geolocation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
-import AdditionalInfo from "./AdditionalInfo";
 
 const Hero = ({info}) => {
-  const [additionalInfo, setAdditionalInfo] = useState(false);
 
-  const handleMoreInfo = () => {
-    setAdditionalInfo(!additionalInfo);
-  }
+  const currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false}).slice(0, 5);
+
+  const iconUrl = `https://openweathermap.org/img/wn/${info.weather[0].icon}@2x.png`
+  
 
   return (
     <>
       <div className="hero">
-      <p className="hero-greeting">Good Morning!</p>
-      <div className="hero-container">
-        <div className="hero-time">{new Date().toLocaleTimeString().slice(0, 5)}</div>
-        <div className="hero-temp">{(info.main.temp - 273.15).toFixed()}°C</div>
+          <p className="hero-greeting">Good {currentTime < 12 ? 'morning' : currentTime < 18 ? 'afternoon' : 'evening'}!</p>
+          <div className="hero-container">
+            <div className="hero-sub">{currentTime}</div>
+            <div className="hero-sub">
+              <img className='hero-img' src={iconUrl} alt="weather icon" />
+            </div>
+            <div className="hero-sub">{(info.main.temp - 273.15).toFixed()}°C</div>
+          </div>
+          <p className="hero-location"><span className="location-text">You are currently in</span> {info.name}, {info.sys.country}</p>
+          <button className='btn btn-hero'>Show More</button>
       </div>
-      <p className="hero-location">You are currently in {info.name}, {info.sys.country}</p>
-      <button className='btn btn-hero' onClick={handleMoreInfo}>Show More</button>
-      </div>
-      {additionalInfo ? <AdditionalInfo info={info}/> : null}
     </>
   )
 }
---
+++ src/components/initial.js
@@ -0,0 +1,48 @@
+const INITIAL = {
+  "coord": {
+      "lon": 10.99,
+      "lat": 44.34
+  },
+  "weather": [
+      {
+          "id": 802,
+          "main": "Clouds",
+          "description": "scattered clouds",
+          "icon": "03d"
+      }
+  ],
+  "base": "stations",
+  "main": {
+      "temp": 291.9,
+      "feels_like": 291.79,
+      "temp_min": 290.19,
+      "temp_max": 294.81,
+      "pressure": 1018,
+      "humidity": 75,
+      "sea_level": 1018,
+      "grnd_level": 934
+  },
+  "visibility": 10000,
+  "wind": {
+      "speed": 2.62,
+      "deg": 61,
+      "gust": 3.54
+  },
+  "clouds": {
+      "all": 36
+  },
+  "dt": 1685018302,
+  "sys": {
+      "type": 2,
+      "id": 2075663,
+      "country": "IT",
+      "sunrise": 1684986007,
+      "sunset": 1685040364
+  },
+  "timezone": 7200,
+  "id": 3163858,
+  "name": "Zocca",
+  "cod": 200
+}
+
+export default INITIAL;
